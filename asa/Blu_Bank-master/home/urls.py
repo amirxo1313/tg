401a62009058da9b2a26757d4736a2a15aa060f8
amirxo1313 <211411871+amirxo1313@users.noreply.github.com>
2025-11-24 20:16:21 +0330

    Delete asa directory

--- asa/Blu_Bank-master/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import charge, show_balance , list_profit ,post_profit
-
-urlpatterns = [
-    path("charge/<int:account_id>/", charge),
-    path("balance/<int:account_id>/", show_balance),
-    path("profits/<int:account_id>/", list_profit),
-    path("add_profit/<int:account_id>/", post_profit)
-]
